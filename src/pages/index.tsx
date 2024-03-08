60d9b94ef16a04419e1bafa8841b941e32c50a55
rafaelroncoletateste <rafaelnh089@gmail.com>
2024-03-08 11:59:31 -0300

    Criando home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
+import Head from "next/head";
+import styles from "../styles/home.module.scss";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <Head>
+        <title>Sujeito Programador</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.containerHeader}>
+          <section className={styles.ctaText}>
+            <h1>Levando você ao próximo nível!</h1>
+            <span>
+              Uma plataforma com cursos que vão do zero até o profissional na
+              prática, direto ao ponto
+            </span>
+            <a>
+              <button>Começar agora!</button>
+            </a>
+
+            <img
+              src="/images/banner-conteudos.png"
+              alt="Conteúdos do Sujeito Programador"
+            />
+          </section>
+        </div>
+      </main>
+    </>
   );
 }
