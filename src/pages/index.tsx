b3f5c3d07c2accfea9370b887def23091291cff2
rafaelroncoletateste <rafaelnh089@gmail.com>
2024-03-08 12:16:12 -0300

    Criando home pt2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,6 +26,33 @@ export default function Home() {
             />
           </section>
         </div>
+
+        <hr className={styles.divisor} />
+
+        <div className={styles.sectionContent}>
+          <section>
+            <h2>Aprenda a criar aplicativos para Android e iOS</h2>
+            <span>
+              Você vai descobrir o jeito mais moderno de desenvolver apps
+              nativos para iOS e Android, construindo aplicativos do zero
+            </span>
+          </section>
+
+          <img src="/images/financasApp.png" alt="Aplicativo de Finanças" />
+        </div>
+
+        <hr className={styles.divisor} />
+
+        <div className={styles.sectionContent}>
+          <img src="/images/webDev.png" alt="Desenvolvimento WEB" />
+          <section>
+            <h2>Aprenda criar sistemas web</h2>
+            <span>
+              Criar sistemas web, sites usando as tecnologias mais mordernas e
+              requisitadas pelo mercado.
+            </span>
+          </section>
+        </div>
       </main>
     </>
   );
